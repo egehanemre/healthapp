da33a861f38c0929cf6f74024b8daac18a729b26
Egehan EMRE <16008122049@ogr.bozok.edu.tr>
2025-01-08 19:46:22 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  eslint: {
+      //todo
+      ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
